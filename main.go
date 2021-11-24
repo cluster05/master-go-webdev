18b566386e254ceb54677385f0ed0e3ab05317a6
cluster05 <ajayakumbhar1234@gmail.com>
2021-11-24 12:48:47 +0530

    render template file [html]

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello home")
+	renderTemplate(rw, "home.html")
 }
 
 func About(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello About")
+	renderTemplate(rw, "about.html")
+
+}
+
+func renderTemplate(rw http.ResponseWriter, templatefileName string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + templatefileName)
+	err := parsedTemplate.Execute(rw, nil)
+	if err != nil {
+		fmt.Println("Error occured while rendering ", templatefileName)
+		return
+	}
 }
 
 func main() {
